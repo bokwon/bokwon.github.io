35e142135e67003e2fa848132b5d0d37ae03b02f
Boyoung Kwon <boyoung.kwon@gmail.com>
2015-11-18 15:13:29 -0800

    add js and footer

---
+++ js/index.js
@@ -0,0 +1,15 @@
+$(document).ready(function(){
+  $('.about').click(function(){
+    $("html, body").animate({scrollTop: 0}, "slow");
+  });
+
+  $('.portfolio').click(function(){
+    var offset = $('#about').height() + 50;
+    $("html, body").animate({scrollTop: offset}, "slow");
+  });
+
+  $('.skills').click(function(){
+    var offset = $('#about').height() + $('#portfolio').height() + 160;
+    $("html, body").animate({scrollTop: offset}, "slow");
+  });
+});
